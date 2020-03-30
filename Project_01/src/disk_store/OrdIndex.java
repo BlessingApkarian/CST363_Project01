44c099845c431b39b38782fbdd0892ffc4dd33c2
blessingapkarian <bapkarian@csumb.edu>
2020-03-29 17:13:26 -0700

    begin lookup & insert + added junit5

--- Project_01/src/disk_store/OrdIndex.java
+++ Project_01/src/disk_store/OrdIndex.java
@@ -41,12 +41,69 @@ public class OrdIndex implements DBIndex {
 	public List<Integer> lookup(int key) {
 		// binary search of entries arraylist
 		// return list of block numbers (no duplicates).
-		// if key not found, return empty list
-		throw new UnsupportedOperationException();
+		// return if key not found, empty list
+
+		List<Integer> result = new ArrayList<>();
+
+		int l = entries.get(0).key;
+		int r = entries.get(entries.size() - 1).key;
+
+		while (l <= r) {
+			int m = l + (r - l) / 2;
+
+			// Check if key is present at mid
+			if (entries.get(m).key == key){
+				for(int i = 0; i < entries.get(m).blocks.size(); i++) {
+					result.add(entries.get(m).blocks.get(i).blockNo);
+				}
+				break;
+			}
+
+			if (entries.get(m).key < key){
+				l = m + 1;
+			} else {
+				r = m - 1;
+			}
+		}
+
+		return result;
 	}
 
 	@Override
 	public void insert(int key, int blockNum) {
+
+		Entry e = new Entry();
+		e.key = key;
+
+		BlockCount b = new BlockCount();
+		b.blockNo = blockNum;
+		b.count = 1;
+
+		e.blocks.add(b);
+
+		int l = entries.get(0).key;
+		int r = entries.get(entries.size() - 1).key;
+
+		while (l <= r) {
+			int m = l + (r - l) / 2;
+
+			// Check if key is present at mid
+			if (entries.get(m).key == key){
+				return;
+			}
+
+			if(key > entries.get(m - 1).key && key < entries.get(m + 1).key){
+				entries.add(m, e);
+				return;
+			}
+
+			if (entries.get(m).key < key){
+				l = m + 1;
+			} else {
+				r = m - 1;
+			}
+		}
+
 		throw new UnsupportedOperationException();
 	}
 
